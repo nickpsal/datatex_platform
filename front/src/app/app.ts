21dc971d41403d3e9a58c39569b48e195a1a542d
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-07-20 17:28:04 +0300

    fix install tailwwind angular material

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+	providers: [
+		provideZonelessChangeDetection(),
+		provideAnimations(),
+	]
+});
