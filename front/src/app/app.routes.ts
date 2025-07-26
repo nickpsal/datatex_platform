b04fdb3f77c7198b89f7431e144db4190dd7892f
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-07-26 21:31:15 +0300

    home page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)
+    },
+    {
         path: 'login',
         loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
     },
     {
         path: 'admin',
         loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.DashboardComponent)
-    },
-    // { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    }
 ];
