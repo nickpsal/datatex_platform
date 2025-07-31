b8824eadf004a6faacb9ae528810c127e789a47f
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-07-31 09:28:04 +0300

    fixes not to conslole log 401 when user is not logged in

--- front/src/app/core/services/auth/auth.ts
+++ front/src/app/core/services/auth/auth.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { map, catchError, of } from 'rxjs';
@@ -19,13 +19,18 @@ export class AuthService {
 	private userSubject = new BehaviorSubject<any | null>(null);
 	user$ = this.userSubject.asObservable();
 
+	private isLoggedIn = signal(false);
+
 	get currentUser() {
 		return this.userSubject.value;
 	}
 
 	login(payload: loginPayload): Observable<any> {
 		return this.api.login(payload).pipe(
-			tap(res => this.userSubject.next(res.user))
+			tap(res => {
+				this.userSubject.next(res.user);
+				this.isLoggedIn.set(true);
+			})
 		);
 	}
 
@@ -40,21 +45,26 @@ export class AuthService {
 	}
 
 	checkAuth(): Observable<boolean> {
-		return this.api.getCurrentUser().pipe(
-			tap(user => this.userSubject.next(user)),
-			map(() => true),
-			catchError(() => {
-				this.userSubject.next(null);
-				return of(false); // ❗ χωρίς redirect εδώ
-			})
-		);
+		if (this.isLoggedIn()) {
+			return this.api.getCurrentUser().pipe(
+				tap(user => this.userSubject.next(user)),
+				map(() => true),
+				catchError(() => {
+					this.userSubject.next(null);
+					return of(false);
+				})
+			);
+		}
+		return of(false);
 	}
 
 	isAdmin(): Observable<boolean> {
-		return this.api.isAdmin().pipe(
-			map(response => response.isAdmin === true),
-			catchError(() => of(false)) // αν αποτύχει (401 ή άλλο), επιστρέφουμε false
-		);
+		if (this.isLoggedIn()) {
+			return this.api.isAdmin().pipe(
+				map(response => response.isAdmin === true),
+				catchError(() => of(false)) // αν αποτύχει (401 ή άλλο), επιστρέφουμε false
+			);
+		}
+		return of(false);
 	}
-
 }
