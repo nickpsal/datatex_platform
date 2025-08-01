a7e1f8e74e5c45f71ece1fe0a09790c52fc227f1
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2025-08-01 10:20:53 +0300

    insert spinner on laoding articles

--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
 import { Article } from '../../core/interfaces/article';
 import { ApiService } from '../../core/services/api/api';
@@ -12,19 +13,24 @@ import { Observable } from 'rxjs';
   imports: [
     MatCardModule,
     NgFor,
+    NgIf,
     RouterModule,
-    AsyncPipe
+    AsyncPipe,
+    MatProgressSpinnerModule
   ],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
 export class HomeComponent implements OnInit {
+  isLoading = signal(true);
+
   articles$: Observable<Article[]> | null = null;
 
-  constructor (private api: ApiService) {}
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
     this.articles$ = this.api.getArticles();
+    this.articles$.subscribe(() => this.isLoading.set(false));
   }
+
 }
-  
